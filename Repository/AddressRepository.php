1158141a39d0124bf5104164f177339b9d93fd33
Raldo CHEA <raldo.chea@gmail.com>
2015-04-03 00:07:20 +0200

    Change DIC about addressRepository

--- Repository/AddressRepository.php
+++ Repository/AddressRepository.php
@@ -2,25 +2,18 @@
 
 namespace Hadonra\Bundle\AddressBundle\Repository;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use Hadonra\Bundle\AddressBundle\Model\AddressInterface;
 
 /**
  * @author Raldo CHEA <raldo@chea.io>
  */
-class AddressRepository
+class AddressRepository extends EntityRepository
 {
-    protected $em;
-
-    public function __construct(EntityManager $em)
-    {
-        $this->em = $em;
-    }
-
     public function attach(AddressInterface $address)
     {
-        $this->em->persist($address);
-        $this->em->flush();
+        $this->_em->persist($address);
+        $this->_em->flush();
 
         return $address;
     }
